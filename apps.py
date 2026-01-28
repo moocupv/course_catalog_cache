aa2732139bee9ad3b045e2ab30903f8ea89b3702
moocupv <mooc@upv.es>
2026-01-28 17:50:22 +0100

    Create apps.py

---
+++ apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CourseCatalogCacheConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "course_catalog_cache"
+    verbose_name = "Course Catalog Cache"
