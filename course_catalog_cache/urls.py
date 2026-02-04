dab8e8f6a0a0357e2db35f67df201324f8538c4f
moocupv <mooc@upv.es>
2026-02-04 13:15:09 +0100

    Update urls.py add refresh endpoint path

--- course_catalog_cache/urls.py
+++ course_catalog_cache/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from .views import courses_all
+from .views import courses_all, courses_refresh
 
 urlpatterns = [
     path("all", courses_all, name="course_catalog_cache_courses_all"),
+    path("refresh", courses_refresh, name="course_catalog_cache_courses_refresh"),
 ]
