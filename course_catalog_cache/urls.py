d7943d54eb434bd9fd5e392f88d5453f07031d89
moocupv <mooc@upv.es>
2026-02-04 18:37:17 +0100

    Update urls.py add trailing slash support

--- course_catalog_cache/urls.py
+++ course_catalog_cache/urls.py
@@ -3,5 +3,7 @@ from .views import courses_all, courses_refresh
 
 urlpatterns = [
     path("all", courses_all, name="course_catalog_cache_courses_all"),
+    path("all/", courses_all, name="course_catalog_cache_courses_all_slash"),
     path("refresh", courses_refresh, name="course_catalog_cache_courses_refresh"),
+    path("refresh/", courses_refresh, name="course_catalog_cache_courses_refresh_slash"),
 ]
